6f84f3f3f0a46bac52504a4eb3a31598f5db9c39
Justin <roguegreat@gmail.com>
2021-05-23 20:39:20 -0500

    Ported over to BepInEx loader.

--- FFAMod/FFAPlugin.cs
+++ FFAMod/FFAPlugin.cs
@@ -1,15 +1,14 @@
-﻿/*
-using BepInEx;
+﻿using BepInEx;
 using HarmonyLib;
 using System.Reflection;
 
 namespace FFAMod
 {
     [BepInPlugin("plugin.bepinex.rounds.all.for.free", "Free For All Mod", "0.5.0.0")]
-    [BepInProcess("ROUNDS.exe")]
+    [BepInProcess("Rounds.exe")]
     public class FFAPlugin : BaseUnityPlugin
     {
-        void Awake()
+        private void Start()
         {
             Logger.LogInfo("Free For All Mod Loaded");
             var harmony = new Harmony("mod.rounds.all.for.free");
@@ -17,4 +16,3 @@ namespace FFAMod
         }
     }
 }
-*/
