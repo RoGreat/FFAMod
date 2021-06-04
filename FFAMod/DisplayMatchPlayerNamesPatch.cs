935124802d20e123b2af6b1034b219e51a0efa3b
bgkillas <55570525+bgkillas@users.noreply.github.com>
2021-06-03 20:55:04 -0400

    Update DisplayMatchPlayerNamesPatch.cs

--- FFAMod/DisplayMatchPlayerNamesPatch.cs
+++ FFAMod/DisplayMatchPlayerNamesPatch.cs
@@ -14,12 +14,22 @@ namespace FFAMod
         {
 			List<Photon.Realtime.Player> list = (from p in PhotonNetwork.CurrentRoom.Players select p.Value).ToList();
 			bool flag = PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey(NetworkConnectionHandler.TWITCH_ROOM_AUDIENCE_RATING_KEY);
-			Player localPlayer = (Player)AccessTools.Method(typeof(PlayerManager), "GetPlayerWithActorID").Invoke(PlayerManager.instance, new object[] { PhotonNetwork.LocalPlayer.ActorNumber });
 			___other.text = null;
+		___local.text=null;
+		Player plr = null;
+					for (int i = 0; i < PlayerManager.instance.players.Count; i++)
+			{
+					if (PlayerManager.instance.players[i].data.view.OwnerActorNr == PhotonNetwork.LocalPlayer.ActorNumber)
+					{
+						plr = PlayerManager.instance.players[i];
+						break;
+					}
+			}
+
 			for (int i = 0; i < list.Count; i++)
 			{
 				string str = flag ? (" (" + list[i].CustomProperties[NetworkConnectionHandler.TWITCH_PLAYER_SCORE_KEY].ToString() + ")") : string.Empty;
-				if (localPlayer.playerID == i)
+				if (plr.playerID == i)
 				{
 					___local.text = list[i].NickName + str;
 				}
