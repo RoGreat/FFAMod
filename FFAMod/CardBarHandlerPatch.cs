8fdbb7326d77178d7a842cc088c18729bb1104e2
bgkillas <55570525+bgkillas@users.noreply.github.com>
2021-06-06 13:30:03 -0400

    Update CardBarHandlerPatch.cs

--- FFAMod/CardBarHandlerPatch.cs
+++ FFAMod/CardBarHandlerPatch.cs
@@ -12,12 +12,12 @@ namespace FFAMod
             CardBarHandler.instance = __instance;
             ___cardBars = __instance.GetComponentsInChildren<CardBar>();
             var bar3 = Object.Instantiate(___cardBars[0], CardBarHandler.instance.transform);
-            bar3.GetChild(0).GetChild(0).GetComponent<ProceduralImage>().color=PlayerSkinBank.GetPlayerSkinColors(2).backgroundColor;
+            bar3.transform.GetChild(0).GetChild(0).gameObject.GetComponent<ProceduralImage>().color=PlayerSkinBank.GetPlayerSkinColors(2).backgroundColor;
             bar3.name = "Bar3";
             bar3.transform.position = ___cardBars[0].transform.position + Vector3.down * 4f;
             bar3.GetComponentInParent<CardBar>();
             var bar4 = Object.Instantiate(___cardBars[1], CardBarHandler.instance.transform);
-            bar4.GetChild(0).GetChild(0).GetComponent<ProceduralImage>().color=PlayerSkinBank.GetPlayerSkinColors(3).backgroundColor;
+            bar4.transform.GetChild(0).GetChild(0).gameObject.GetComponent<ProceduralImage>().color=PlayerSkinBank.GetPlayerSkinColors(3).backgroundColor;
             bar4.name = "Bar4";
             bar4.transform.position = ___cardBars[1].transform.position + Vector3.down * 4f;
             bar4.GetComponentInParent<CardBar>();
